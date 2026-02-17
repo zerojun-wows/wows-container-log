29de3ab2e5901a6570127e011479debfecba0c73
zerojun <zerojun@gmx.net>
2026-02-17 02:33:50 +0100

    Change smaller typo corrections

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-""" 
+"""
 This module is the entry point of the application.
 """
+
 from PySide6.QtWidgets import QApplication
 
 from src.gui.main_window import MainWindow
 
 import sys
 
+
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     app.exec()
+
 
 if __name__ == "__main__":
     main()
